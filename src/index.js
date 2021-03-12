501c856859f385c9b7e94e9126dcd9fcec17acb3
Cy-real <79482914+Cy-real@users.noreply.github.com>
2021-03-12 22:01:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 module.exports = function reverse (n) 
 {
-  let tmp = String(n).split('').reverse().join('');
-//  let result = [];
- // for(let i=tmp.length-1;i>=0;i--)
- //   {
- //     result.push(tmp[i]);
- //   }
- //   result = result.join('');
- //   return result;
-    return Number(tmp);
+  let result = n.toString().split('').reverse().join('').toNumber();
+    return result;
 }
